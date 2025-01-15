ade19a886c5186ee265aafe82ecab54e087ee245
Sithum Bimsara <sithum.22@cse.mrt.ac.lk>
2025-01-15 19:57:57 +0530

    created ProductDetailsDTO

---
+++ E-com-backend/src/main/java/com/ecom/backend/DTO/ProductDetailsRequest.java
@@ -0,0 +1,28 @@
+package com.ecom.backend.DTO;
+
+import com.ecom.backend.model.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class ProductDetailsRequest {
+    private String id;
+    private String name;
+    private double price;
+    private double rating; // (0-5)
+    private String brand;
+    private String category;
+    private String subCategory;
+    private List<String> tags;
+    private int inventoryCount; // Number of items left
+    private List<String> otherImages;
+    private String description;
+    private List<Product.Spec> specs;
+
+}
+
+
