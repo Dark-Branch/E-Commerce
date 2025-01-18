e1fe4dedc02c11639c647391d7c7679e6619808a
Ke-vin-S <kevinxsanjula@gmail.com>
2025-01-18 23:04:20 +0530

    fix Errors

--- E-com-backend/src/test/java/com/ecom/backend/service/ProductServiceTest.java
+++ E-com-backend/src/test/java/com/ecom/backend/service/ProductServiceTest.java
@@ -1,6 +1,5 @@
 package com.ecom.backend.service;
 
-
 import com.ecom.backend.model.Product;
 import com.ecom.backend.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
@@ -8,6 +7,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
@@ -33,7 +34,12 @@ public class ProductServiceTest {
         when(productRepository.save(product)).thenReturn(product);
 
         Product createdProduct = productService.createProduct(product);
+
         assertEquals("123456", createdProduct.getId());
         assertEquals("Wireless Bluetooth Headphones", createdProduct.getName());
+        assertEquals(59.99, createdProduct.getPrice());
+        assertEquals(4.5, createdProduct.getRating());
+        assertEquals("https://example.com/images/headphones-main.jpg", createdProduct.getMainImg());
+        assertEquals(15, createdProduct.getInventoryCount());
     }
 }
