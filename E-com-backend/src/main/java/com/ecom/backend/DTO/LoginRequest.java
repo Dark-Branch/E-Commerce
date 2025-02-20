60e308fed490930f3f0b46cd33dda3b6705a10cf
Ke-vin-S <kevinxsanjula@gmail.com>
2025-02-20 22:31:41 +0530

    Product endpoint

--- E-com-backend/src/main/java/com/ecom/backend/DTO/LoginRequest.java
+++ E-com-backend/src/main/java/com/ecom/backend/DTO/LoginRequest.java
@@ -1,5 +1,6 @@
 package com.ecom.backend.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
 public class LoginRequest {
     private String email;
     private String password;
