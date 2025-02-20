bb5bf643c4d964b10e8004d42570f2e7519bd85b
Ke-vin-S <kevinxsanjula@gmail.com>
2025-02-20 21:38:54 +0530

    DTO for auth

---
+++ E-com-backend/src/main/java/com/ecom/backend/DTO/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.ecom.backend.DTO;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class LoginRequest {
+    private String email;
+    private String password;
+}
