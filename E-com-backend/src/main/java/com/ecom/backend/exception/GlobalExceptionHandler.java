47b4c44ef4a18b3bb0e96aa0bd8d9bc5dd403dce
Ke-vin-S <kevinxsanjula@gmail.com>
2025-02-20 22:55:29 +0530

    Exceptions handling

--- E-com-backend/src/main/java/com/ecom/backend/exception/GlobalExceptionHandler.java
+++ E-com-backend/src/main/java/com/ecom/backend/exception/GlobalExceptionHandler.java
@@ -4,8 +4,17 @@ import com.mongodb.DuplicateKeyException;
 import org.springframework.data.mongodb.core.mapping.ExplicitEncrypted;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -34,4 +43,35 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleGeneralException(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.");
     }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<Map<String, Object>> handleUnauthorizedException(UnauthorizedException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.UNAUTHORIZED.value());
+        response.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+
+        List<ObjectError> globalErrors = ex.getBindingResult().getGlobalErrors();
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+        if (!globalErrors.isEmpty()) {
+            response.put("globalErrors", globalErrors);
+        }
+
+        if (!fieldErrors.isEmpty()) {
+            response.put("fieldErrors", fieldErrors);
+        }
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
 }
